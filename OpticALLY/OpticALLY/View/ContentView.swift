d61f3e0f1ba55f2861e053aa695df6acbefb1156
John Seong <wonmor@gmail.com>
2023-11-21 14:47:35 -0800

    Update ContentView.swift

--- OpticALLY/OpticALLY/View/ContentView.swift
+++ OpticALLY/OpticALLY/View/ContentView.swift
@@ -49,5 +49,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environmentObject(GlobalState())
+            .preferredColorScheme(.dark)
     }
 }
