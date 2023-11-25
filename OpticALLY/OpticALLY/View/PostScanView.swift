9c029aea6abedcc8e673f53a496c9cbc8a83b77c
John Seong <wonmor@gmail.com>
2023-11-25 15:43:06 -0800

    Refactoring change 5

--- OpticALLY/OpticALLY/View/PostScanView.swift
+++ OpticALLY/OpticALLY/View/PostScanView.swift
@@ -10,67 +10,6 @@ import SceneKit
 import ModelIO
 import SceneKit.ModelIO
 
-struct SceneKitView: UIViewRepresentable {
-    var geometry: SCNGeometry?
-    
-    func makeUIView(context: Context) -> SCNView {
-        let scnView = SCNView()
-        
-        // Configure the scene
-        let scene = SCNScene()
-        scnView.scene = scene
-        
-        // Check if geometry is provided
-        if let geometry = geometry {
-            let node = SCNNode(geometry: geometry)
-            node.position = SCNVector3(x: 0, y: 0, z: 0)
-            
-            node.eulerAngles.z = .pi / -2
-            
-            scene.rootNode.addChildNode(node)
-        }
-        
-        scnView.autoenablesDefaultLighting = true
-        scnView.allowsCameraControl = true
-        
-        return scnView
-    }
-    
-    func updateUIView(_ uiView: SCNView, context: Context) {}
-}
-
-struct SceneKitMDLView: UIViewRepresentable {
-    var mdlAsset: MDLAsset
-
-    func makeUIView(context: Context) -> SCNView {
-        let scnView = SCNView()
-
-        // Configure the scene
-        let scene = SCNScene()
-        scnView.scene = scene
-        
-        if let object = mdlAsset.object(at: 0) as? MDLMesh, let geometry: SCNGeometry? = SCNGeometry(mdlMesh: object) {
-            // Modify each material to be double-sided
-            geometry!.materials.forEach { material in
-                material.isDoubleSided = true
-            }
-
-            let node = SCNNode(geometry: geometry)
-            node.position = SCNVector3(x: 0, y: 0, z: 0)
-            node.eulerAngles.z = .pi / -2
-
-            scene.rootNode.addChildNode(node)
-        }
-
-        scnView.autoenablesDefaultLighting = true
-        scnView.allowsCameraControl = true
-
-        return scnView
-    }
-
-    func updateUIView(_ uiView: SCNView, context: Context) {}
-}
-
 struct PostScanView: View {
     @EnvironmentObject var globalState: GlobalState
     
