31768d0cd30cba86c6ca6400eacb1feb077ecb52
John Seong <wonmor@gmail.com>
2023-11-25 16:03:29 -0800

    Added checkmark and haptics upon conversion completion

--- OpticALLY/OpticALLY/View/PostScanView.swift
+++ OpticALLY/OpticALLY/View/PostScanView.swift
@@ -17,101 +17,126 @@ struct PostScanView: View {
     
     @State private var isMesh: Bool = false
     @State private var triggerUpdate: Bool = false
+    @State private var showCompletionCheckmark = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    ExternalData.reset()
-                    globalState.currentView = .scanning
-                }) {
-                    Image(systemName: "arrow.left") // You can customize this with your own back button image
-                        .foregroundStyle(.white)
-                        .font(.title)
-                        .padding()
-                }
-                
-                Text("Your Scan")
-                    .font(.system(.title)) // Using monospaced font
-                    .monospaced()
-                Spacer()
+        ZStack {
+            if showCompletionCheckmark {
+                CheckmarkView()
+                    .opacity(1) // Make visible
+                    .scaleEffect(1) // Scale to normal size
+                    .transition(.scale) // Add a transition effect
             }
-            .padding(.top)
-            
-            if triggerUpdate {
-                SceneKitMDLView(mdlAsset: MDLAsset(url: exportViewModel.fileURL!))
-                    .onAppear(perform: {
-                        print("File URL: \(exportViewModel.fileURL!)")
-                    })
-                
-            } else {
-                SceneKitView(geometry: ExternalData.pointCloudGeometry)
-                    .ignoresSafeArea(edges: .bottom)
-            }
-            
-            Spacer()
-            
-            if !isMesh {
-                Button(action: {
-                    exportViewModel.exportOBJ()
-                    isMesh = true
-                }) {
-                    HStack(spacing: 10) {
-                        Image(systemName: "square.stack.3d.forward.dottedline.fill")
-                        
-                        Text("Convert to Mesh")
-                            .bold()
-                            .font(.title3)
+            VStack {
+                HStack {
+                    Button(action: {
+                        ExternalData.reset()
+                        globalState.currentView = .scanning
+                    }) {
+                        Image(systemName: "arrow.left") // You can customize this with your own back button image
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
                     }
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Capsule().fill(Color(.darkGray)))
-                }
-                .padding()
-                
-            } else if !exportViewModel.isLoading {
-                Text("Surface Reconstruct\nComplete!")
-                    .monospaced()
-                    .padding()
-                    .multilineTextAlignment(.center)
-            }
-            
-            // Display a loading spinner when isLoading is true
-            if exportViewModel.isLoading {
-                VStack(spacing: 10) { // Adjust spacing as needed
-                    ProgressView()
-                        .scaleEffect(1.5, anchor: .center) // Adjust size as needed
-                        .progressViewStyle(CircularProgressViewStyle(tint: .white)) // Spinner color
-                        .padding()
                     
-                    Text("PROCESSING POINT CLOUD")
-                        .bold()
+                    Text("Your Scan")
+                        .font(.system(.title)) // Using monospaced font
                         .monospaced()
-                        .foregroundColor(.white)
-                    
-                    if exportViewModel.estimatedExportTime != nil {
-                        Text("Estimated:\n\(exportViewModel.estimatedExportTime!) sec.")
-                            .monospaced()
-                            .foregroundColor(.white)
-                    } else {
-                        Text("Estimated:\nN/A")
-                            .monospaced()
-                            .foregroundColor(.white)
-                    }
+                    Spacer()
                 }
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 30) // Adjust horizontal padding for wider background
-                .padding(.vertical, 15) // Adjust vertical padding for background height
-                .zIndex(1) // Ensure the spinner and text are above other content
-                .onDisappear() {
-                    // Upon .OBJ conversion completion, refresh SceneKitView
-                    triggerUpdate = true
+                .padding(.top)
+                
+                if triggerUpdate {
+                    SceneKitMDLView(mdlAsset: MDLAsset(url: exportViewModel.fileURL!))
+                        .onAppear(perform: {
+                            print("File URL: \(exportViewModel.fileURL!)")
+                        })
+                    
+                } else {
+                    SceneKitView(geometry: ExternalData.pointCloudGeometry)
+                        .ignoresSafeArea(edges: .bottom)
+                }
+                
+                Spacer()
+                
+                if !isMesh {
+                    Button(action: {
+                        exportViewModel.exportOBJ()
+                        isMesh = true
+                    }) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "square.stack.3d.forward.dottedline.fill")
+                            
+                            Text("Convert to Mesh")
+                                .bold()
+                                .font(.title3)
+                        }
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Capsule().fill(Color(.darkGray)))
+                    }
+                    .padding()
+                    
+                } else if !exportViewModel.isLoading {
+                    Text("Surface Reconstruct\nComplete!")
+                        .monospaced()
+                        .padding()
+                        .multilineTextAlignment(.center)
+                }
+                
+                // Display a loading spinner when isLoading is true
+                if exportViewModel.isLoading {
+                    VStack(spacing: 10) { // Adjust spacing as needed
+                        ProgressView()
+                            .scaleEffect(1.5, anchor: .center) // Adjust size as needed
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white)) // Spinner color
+                            .padding()
+                        
+                        Text("PROCESSING POINT CLOUD")
+                            .bold()
+                            .monospaced()
+                            .foregroundColor(.white)
+                        
+                        if exportViewModel.estimatedExportTime != nil {
+                            Text("Estimated:\n\(exportViewModel.estimatedExportTime!) sec.")
+                                .monospaced()
+                                .foregroundColor(.white)
+                        } else {
+                            Text("Estimated:\nN/A")
+                                .monospaced()
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30) // Adjust horizontal padding for wider background
+                    .padding(.vertical, 15) // Adjust vertical padding for background height
+                    .zIndex(1) // Ensure the spinner and text are above other content
+                    .onDisappear {
+                        triggerUpdate = true
+                        
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1)) {
+                            showCompletionCheckmark = true
+                        }
+                        
+                        let generator = UINotificationFeedbackGenerator()
+                        generator.notificationOccurred(.success)
+                    }
                 }
             }
         }
     }
 }
 
+struct CheckmarkView: View {
+    var body: some View {
+        Image(systemName: "checkmark.circle.fill")
+            .resizable()
+            .frame(width: 60, height: 60)
+            .foregroundColor(.green)
+            .opacity(0) // Initially invisible
+            .scaleEffect(0) // Initially scaled down
+    }
+}
 
 struct PostScanView_Previews: PreviewProvider {
     static var previews: some View {
