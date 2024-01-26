aa4dfc0531b56a2a784e2d372fa19178eb6b1c0c
John Seong <wonmor@gmail.com>
2024-01-26 13:24:25 -0800

    Another refactor

--- OpticALLY/OpticALLY/OpticALLY/OpticALLYApp.swift
+++ OpticALLY/OpticALLY/OpticALLY/OpticALLYApp.swift
@@ -89,7 +89,7 @@ struct OpticALLYApp: App {
         }
     }
     
-    static func ballPivotingSurfaceReconstructionPLYtoOBJ(fileURL: URL) throws -> URL {
+    static func ballPivotingSurfaceReconstruction_PLYtoOBJ(fileURL: URL) throws -> URL {
         let fileManager = FileManager.default
         let tempDir = fileManager.temporaryDirectory.appendingPathComponent("temp", isDirectory: true)
         
