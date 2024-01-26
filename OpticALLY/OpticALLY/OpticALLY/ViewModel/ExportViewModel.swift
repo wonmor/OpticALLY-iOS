fc0141031913863c237ab1e9c2c992de4492d5a7
John Seong <wonmor@gmail.com>
2024-01-26 13:24:25 -0800

    Another Refactor

--- OpticALLY/OpticALLY/OpticALLY/ViewModel/ExportViewModel.swift
+++ OpticALLY/OpticALLY/OpticALLY/ViewModel/ExportViewModel.swift
@@ -177,7 +177,7 @@ class ExportViewModel: ObservableObject {
                   PyGILState_Release(gstate)
               }
                 // Call convertToObj, which now handles both PLY export and OBJ conversion
-                let objFileURL = try OpticALLYApp.ballPivotingSurfaceReconstructionPLYtoOBJ(fileURL: plyFileURL)
+                let objFileURL = try OpticALLYApp.ballPivotingSurfaceReconstruction_PLYtoOBJ(fileURL: plyFileURL)
 
                 // Update the UI with the results of the export process
                 DispatchQueue.main.async {
