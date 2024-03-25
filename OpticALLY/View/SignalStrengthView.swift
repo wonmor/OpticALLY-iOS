c76e5ed2a00114cca08282c78647f964f7e9b896
John Seong <wonmor@gmail.com>
2024-03-25 16:05:21 -0700

    Update SignalStrengthView.swift

--- OpticALLY/View/SignalStrengthView.swift
+++ OpticALLY/View/SignalStrengthView.swift
@@ -12,18 +12,103 @@ struct SignalStrengthView: View {
     
     @ObservedObject var cameraViewController: CameraViewController
     
+    private func determineActive1() -> Int {
+        switch scanDirection {
+        case .left:
+            let diff = abs(20 - cameraViewController.faceYawAngle)
+            
+            if diff >= 15 {
+                return 0
+                
+            } else if diff >= 10 {
+                return 1
+                
+            } else if diff >= 5 {
+                return 2
+                
+            } else if diff >= 2.5 {
+                return 3
+            }
+            
+        case .front where abs(cameraViewController.faceYawAngle) < 10:
+            return -1
+            
+        case .right where cameraViewController.faceYawAngle < -20:
+            return -1
+            
+        default:
+            return -1
+        }
+        
+        return -1
+    }
+    
+    private func determineActive2() -> Int {
+        switch scanDirection {
+        case .left where cameraViewController.faceYawAngle > 20:
+            return -1
+            
+        case .front where abs(cameraViewController.faceYawAngle) < 10:
+            let diff = abs(0 - cameraViewController.faceYawAngle)
+            
+            if diff >= 15 {
+                return 0
+                
+            } else if diff >= 10 {
+                return 1
+                
+            } else if diff >= 5 {
+                return 2
+                
+            } else if diff >= 2.5 {
+                return 3
+            }
+            
+        case .right where cameraViewController.faceYawAngle < -20:
+            let diff = abs(-20 - cameraViewController.faceYawAngle)
+            
+            if diff >= 15 {
+                return 0
+                
+            } else if diff >= 10 {
+                return 1
+                
+            } else if diff >= 5 {
+                return 2
+                
+            } else if diff >= 2.5 {
+                return 3
+            }
+            
+        default:
+            return -1
+        }
+        
+        return -1
+    }
+    
     var body: some View {
         ZStack {
             HStack(spacing: -225) {
                 ForEach(0..<5) { index in
-                    SignalArc(index: index)
+                    if index <= determineActive1() {
+                        SignalArc(index: index, isActive: true)
+                        
+                    } else {
+                        SignalArc(index: index, isActive: false)
+                    }
                 }
                 
             }
             
             HStack(spacing: -225) {
                 ForEach(0..<5) { index in
-                    SignalArc(index: index)
+                    if index <= determineActive2() {
+                        SignalArc(index: index, isActive: true)
+                        
+                    } else {
+                        SignalArc(index: index, isActive: false)
+                    }
                 }
             }
             .scaleEffect(x: -1, y: 1)
@@ -33,6 +118,7 @@ struct SignalStrengthView: View {
 
 struct SignalArc: View {
     let index: Int
+    let isActive: Bool
     
     var body: some View {
         GeometryReader { geometry in
@@ -51,7 +137,7 @@ struct SignalArc: View {
                     clockwise: false
                 )
             }
-            .stroke(.gray, lineWidth: 2)
+            .stroke(isActive ? .green : .gray, lineWidth: 2)
         }
     }
 }
