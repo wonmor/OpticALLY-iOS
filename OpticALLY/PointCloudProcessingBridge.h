e9311981ce25ade369297fb93ab817cad76faa84
John Seong <wonmor@gmail.com>
2024-05-09 13:04:34 -0700

    Added C++ PointCloudProcessingBridge

---
+++ OpticALLY/PointCloudProcessingBridge.h
@@ -0,0 +1,13 @@
+//
+//  PointCloudProcessingBridge.h
+//  OpticALLY
+//
+//  Created by John Seong on 5/9/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PointCloudProcessingBridge : NSObject
+
+@end
---
+++ OpticALLY/PointCloudProcessingBridge.mm
@@ -0,0 +1,18 @@
+//
+//  PointCloudProcessingBridge.m
+//  OpticALLY
+//
+//  Created by John Seong on 5/9/24.
+//
+
+
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgcodecs/ios.h>
+#import <Foundation/Foundation.h>
+#import "PointCloudProcessingBridge.h"
+#include "PointCloudProcessing.hpp"
+
+@implementation PointCloudProcessingBridge
+
+
+@end
