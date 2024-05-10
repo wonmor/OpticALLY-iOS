3b2117ebb850eca134b4dea38ccd166cc82b61f3
John Seong <wonmor@gmail.com>
2024-05-09 22:50:10 -0700

    Added Objective-C++ PointCloudProcessingBrdige

--- OpticALLY/PointCloudProcessingBridge.h
+++ OpticALLY/PointCloudProcessingBridge.h
@@ -1,13 +1,15 @@
-//
-//  PointCloudProcessingBridge.h
-//  OpticALLY
-//
-//  Created by John Seong on 5/9/24.
-//
+// PointCloudProcessingBridge.h
+// OpticALLY
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface PointCloudProcessingBridge : NSObject
+
+// Exposes a method to Swift that can take all necessary parameters for processing point clouds.
++ (BOOL)processPointCloudsWithCalibrationFile:(NSString *)calibrationFilePath
+                                  imageFiles:(NSArray<NSString *> *)imageFiles
+                                  depthFiles:(NSArray<NSString *> *)depthFiles
+                                  outputPath:(NSString *)outputPath;
 
 @end
--- OpticALLY/PointCloudProcessingBridge.mm
+++ OpticALLY/PointCloudProcessingBridge.mm
@@ -1,18 +1,35 @@
-//
-//  PointCloudProcessingBridge.m
-//  OpticALLY
-//
-//  Created by John Seong on 5/9/24.
-//
+// PointCloudProcessingBridge.mm
+// OpticALLY
 
-
-#import <opencv2/opencv.hpp>
-#import <opencv2/imgcodecs/ios.h>
-#import <Foundation/Foundation.h>
 #import "PointCloudProcessingBridge.h"
-#include "PointCloudProcessing.hpp"
+#include "PointCloudProcessing.hpp" // Include the C++ header that has the processing function.
 
 @implementation PointCloudProcessingBridge
 
++ (BOOL)processPointCloudsWithCalibrationFile:(NSString *)calibrationFilePath
+                                   imageFiles:(NSArray<NSString *> *)imageFiles
+                                   depthFiles:(NSArray<NSString *> *)depthFiles
+                                   outputPath:(NSString *)outputPath {
+    // Convert NSArray to std::vector because our C++ code expects std::vector
+    std::vector<std::string> cppImageFiles;
+    for (NSString *path in imageFiles) {
+        cppImageFiles.push_back([path UTF8String]);
+    }
+
+    std::vector<std::string> cppDepthFiles;
+    for (NSString *path in depthFiles) {
+        cppDepthFiles.push_back([path UTF8String]);
+    }
+
+    // Call the C++ function
+    try {
+        processPointCloudsToObj([calibrationFilePath UTF8String], cppImageFiles, cppDepthFiles, [outputPath UTF8String]);
+    } catch (const std::exception &e) {
+        NSLog(@"C++ Exception: %s", e.what());
+        return NO; // Return NO in case of failure
+    }
+
+    return YES; // Return YES if the processing is successful
+}
 
 @end
