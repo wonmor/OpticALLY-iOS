b5066f1eb68f7658017c7c2460df6aca8812f951
John Seong <wonmor@gmail.com>
2024-05-10 11:06:17 -0700

    In the process of debugging On-Device Meshing...

--- OpticALLY/PointCloudProcessingBridge.h
+++ OpticALLY/PointCloudProcessingBridge.h
@@ -1,15 +1,16 @@
 // PointCloudProcessingBridge.h
-// OpticALLY
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface PointCloudProcessingBridge : NSObject
 
-// Exposes a method to Swift that can take all necessary parameters for processing point clouds.
 + (BOOL)processPointCloudsWithCalibrationFile:(NSString *)calibrationFilePath
-                                  imageFiles:(NSArray<NSString *> *)imageFiles
-                                  depthFiles:(NSArray<NSString *> *)depthFiles
-                                  outputPath:(NSString *)outputPath;
+                                   imageFiles:(NSArray<NSString *> *)imageFiles
+                                   depthFiles:(NSArray<NSString *> *)depthFiles
+                                   outputPath:(NSString *)outputPath;
 
 @end
+
+NS_ASSUME_NONNULL_END
