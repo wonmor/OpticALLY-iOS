ca33b60564bb730c3c0bbf915979e024289e4523
John Seong <wonmor@gmail.com>
2024-07-20 11:02:16 +0900

    Added convertScaleCGRect

--- OpticALLY/DlibWrapper.mm
+++ OpticALLY/DlibWrapper.mm
@@ -1,5 +1,14 @@
+//
+//  DlibWrapper.m
+//  DisplayLiveSamples
+//
+//  Created by Luis Reisewitz on 16.05.16.
+//  Copyright © 2016 ZweiGraf. All rights reserved.
+//
+
 #import "DlibWrapper.h"
 #import <UIKit/UIKit.h>
+
 #include <dlib/image_processing.h>
 #include <dlib/image_io.h>
 
@@ -7,15 +16,16 @@
 
 @property (assign) BOOL prepared;
 
-+ (std::vector<dlib::rectangle>)convertCGRectValueArray:(NSArray<NSValue *> *)rects;
++ (dlib::rectangle)convertScaleCGRect:(CGRect)rect toDlibRectacleWithImageSize:(CGSize)size;
++ (std::vector<dlib::rectangle>)convertCGRectValueArray:(NSArray<NSValue *> *)rects toVectorWithImageSize:(CGSize)size;
 
 @end
-
 @implementation DlibWrapper {
     dlib::shape_predictor sp;
 }
 
-- (instancetype)init {
+
+-(instancetype)init {
     self = [super init];
     if (self) {
         _prepared = NO;
@@ -25,38 +35,23 @@
 
 - (void)prepare {
     NSString *modelFileName = [[NSBundle mainBundle] pathForResource:@"shape_predictor_68_face_landmarks" ofType:@"dat"];
-    if (!modelFileName) {
-        NSLog(@"Model file not found in the bundle.");
-        return;
-    }
+    std::string modelFileNameCString = [modelFileName UTF8String];
     
-    const char *modelFileNameCString = [modelFileName UTF8String];
-    if (!modelFileNameCString) {
-        NSLog(@"Failed to convert NSString to C string.");
-        return;
-    }
+    dlib::deserialize(modelFileNameCString) >> sp;
     
-    try {
-        dlib::deserialize(modelFileNameCString) >> sp;
-        self.prepared = YES;
-        NSLog(@"DLib Wrapper Loading Completed!");
-        
-    } catch (const std::exception& e) {
-        NSLog(@"Deserialization failed: %s", e.what());
-    }
+    // FIXME: test this stuff for memory leaks (cpp object destruction)
+    self.prepared = YES;
 }
 
-- (void)doWorkOnSampleBuffer:(CMSampleBufferRef)sampleBuffer inRects:(NSArray<NSValue *> *)rects {
+-(void)doWorkOnSampleBuffer:(CMSampleBufferRef)sampleBuffer inRects:(NSArray<NSValue *> *)rects {
+    
     if (!self.prepared) {
         [self prepare];
-        if (!self.prepared) {
-            NSLog(@"Failed to prepare shape predictor.");
-            return;
-        }
     }
     
     dlib::array2d<dlib::bgr_pixel> img;
     
+    // MARK: magic
     CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
     CVPixelBufferLockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
 
@@ -64,17 +59,22 @@
     size_t height = CVPixelBufferGetHeight(imageBuffer);
     char *baseBuffer = (char *)CVPixelBufferGetBaseAddress(imageBuffer);
     
+    // set_size expects rows, cols format
     img.set_size(height, width);
     
+    // copy samplebuffer image data into dlib image format
     img.reset();
     long position = 0;
     while (img.move_next()) {
         dlib::bgr_pixel& pixel = img.element();
 
-        long bufferLocation = position * 4;
+        // assuming bgra format here
+        long bufferLocation = position * 4; //(row * width + column) * 4;
         char b = baseBuffer[bufferLocation];
         char g = baseBuffer[bufferLocation + 1];
         char r = baseBuffer[bufferLocation + 2];
+        //        we do not need this
+        //        char a = baseBuffer[bufferLocation + 3];
         
         dlib::bgr_pixel newpixel(b, g, r);
         pixel = newpixel;
@@ -82,58 +82,66 @@
         position++;
     }
     
+    // unlock buffer again until we need it again
     CVPixelBufferUnlockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
+
+    CGSize imageSize = CGSizeMake(width, height);
     
-    std::vector<dlib::rectangle> convertedRectangles = [DlibWrapper convertCGRectValueArray:rects];
+    // convert the face bounds list to dlib format
+    std::vector<dlib::rectangle> convertedRectangles = [DlibWrapper convertCGRectValueArray:rects toVectorWithImageSize:imageSize];
     
-    for (unsigned long j = 0; j < convertedRectangles.size(); ++j) {
+    // for every detected face
+    for (unsigned long j = 0; j < convertedRectangles.size(); ++j)
+    {
         dlib::rectangle oneFaceRect = convertedRectangles[j];
         
+        // detect all landmarks
         dlib::full_object_detection shape = sp(img, oneFaceRect);
         
+        // and draw them into the image (samplebuffer)
         for (unsigned long k = 0; k < shape.num_parts(); k++) {
             dlib::point p = shape.part(k);
             draw_solid_circle(img, p, 3, dlib::rgb_pixel(0, 255, 255));
         }
-        
-        // Print out eye coordinates
-        for (unsigned long k = 36; k <= 47; k++) {
-            dlib::point p = shape.part(k);
-            if (k <= 41) {
-                NSLog(@"Left eye point %lu: (%ld, %ld)", k - 35, p.x(), p.y());
-            } else {
-                NSLog(@"Right eye point %lu: (%ld, %ld)", k - 41, p.x(), p.y());
-            }
-        }
     }
     
+    // lets put everything back where it belongs
     CVPixelBufferLockBaseAddress(imageBuffer, 0);
 
+    // copy dlib image data back into samplebuffer
     img.reset();
     position = 0;
     while (img.move_next()) {
         dlib::bgr_pixel& pixel = img.element();
         
-        long bufferLocation = position * 4;
+        // assuming bgra format here
+        long bufferLocation = position * 4; //(row * width + column) * 4;
         baseBuffer[bufferLocation] = pixel.blue;
         baseBuffer[bufferLocation + 1] = pixel.green;
         baseBuffer[bufferLocation + 2] = pixel.red;
+        //        we do not need this
+        //        char a = baseBuffer[bufferLocation + 3];
         
         position++;
     }
     CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
 }
 
-+ (std::vector<dlib::rectangle>)convertCGRectValueArray:(NSArray<NSValue *> *)rects {
++ (dlib::rectangle)convertScaleCGRect:(CGRect)rect toDlibRectacleWithImageSize:(CGSize)size {
+    long left = rect.origin.x * size.width;
+    long top = rect.origin.y * size.height;
+    long right = (rect.origin.x + rect.size.width) * size.width;
+    long bottom = (rect.origin.y + rect.size.height) * size.height;
+    
+    dlib::rectangle dlibRect(left, top, right, bottom);
+    return dlibRect;
+}
+
++ (std::vector<dlib::rectangle>)convertCGRectValueArray:(NSArray<NSValue *> *)rects toVectorWithImageSize:(CGSize)size {
     std::vector<dlib::rectangle> myConvertedRects;
     for (NSValue *rectValue in rects) {
-        CGRect rect = [rectValue CGRectValue];
-        long left = rect.origin.x;
-        long top = rect.origin.y;
-        long right = left + rect.size.width;
-        long bottom = top + rect.size.height;
-        dlib::rectangle dlibRect(left, top, right, bottom);
-
+        CGRect singleRect = [rectValue CGRectValue];
+        dlib::rectangle dlibRect = [DlibWrapper convertScaleCGRect:singleRect toDlibRectacleWithImageSize:size];
         myConvertedRects.push_back(dlibRect);
     }
     return myConvertedRects;
