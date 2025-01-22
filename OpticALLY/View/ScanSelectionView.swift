efe898589660705bbb2c7c802f7841842f6b5a69
John Seong <wonmor@gmail.com>
2025-01-22 16:55:57 +0900

    Added ScanSelectionView

---
+++ OpticALLY/View/ScanSelectionView.swift
@@ -0,0 +1,16 @@
+//
+//  ScanSelectionView.swift
+//  OpticALLY
+//
+//  Created by John Seong on 1/22/25.
+//
+
+import SwiftUI
+
+struct ScanSelectionView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
